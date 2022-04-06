34c9f3d8476e3c6ec5968e9f1b05c2f87641caf1
PokazannikovMS <mp@intes.pro>
2022-04-06 21:48:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ headers = {
     }
 
 def get_articles(url):
-    print("hello")
+    #print("hello")
     s = requests.session()
     response = s.get(url=url, headers=headers)
     soup = BS(response.text, 'lxml')
     items = soup.find_all('div', class_='search-registry-entry-block')
-    print(len(items))
+    print(f'Получено {len(items)} записей.')
+    for i in items:
+        types_law = i.find('div', class_='registry-entry__header-top__title').text.strip().split('\n')
+        types = types_law[0].strip()
+        law = types_law[1].strip()
+        print(f'|{types}_{law}|')
+                                    
+                                
 
 def main():
-    print("hello")
+    #print("hello")
     get_articles(url="https://zakupki.gov.ru/epz/order/extendedsearch/results.html?searchString&morphology=on&strictEqual=on&openMode=USE_DEFAULT_PARAMS&pageNumber=1&sortDirection=false&recordsPerPage=_10&showLotsInfoHidden=false&fz44=on&fz223=on&fz94=on&orderNumber&placingWaysList&placingWaysList223&priceFrom&priceTo&currencyId&participantName&publishDateFrom&publishDateTo&updateDateFrom&updateDateTo&customerTitle&customerCode&customerFz94id&customerFz223id&customerInn&agencyTitle=+...++%D0%97%D0%94%D0%A0%D0%90%D0%92%D0%9E%D0%9E%D0%A5%D0%A0%D0%90%D0%9D%D0%95%D0%9D%D0%98%D0%AF+%22%D0%A6%D0%95%D0%9D%D0%A2%D0%A0%D0%90%D0%9B%D0%AC%D0%9D%D0%90%D0%AF+%D0%93%D0%9E%D0%A0%D0%9E%D0%94%D0%A1%D0%9A%D0%90%D0%AF+%D0%91%D0%9E%D0%9B%D0%AC%D0%9D%D0%98%D0%A6%D0%90%22+%D0%93%D0%9E%D0%A0%D0%9E%D0%94%D0%90+%D0%94%D0%9E%D0%9D%D0%95%D0%A6%D0%9A%D0%90+%D0%A0%D0%9E%D0%A1%D0%A2%D0%9E%D0%92%D0%A1%D0%9A%D0%9E%D0%99+%D0%9E%D0%91%D0%9B%D0%90%D0%A1%D0%A2%D0%98&agencyCode=03583000707&agencyFz94id=688357&agencyFz223id&agencyInn=6145000407&districts&regions&af=on&ca=on&deliveryAddress&sortBy=UPDATE_DATE#")
 
 if __name__ == "__main__":
